69f0780261d6a14d492f9d94598e2f87b401bafb
manugando <emanuele.gandolfi89@gmail.com>
2016-07-03 16:39:02 +0200

    Added dependencies and initial configuration

---
+++ src/client/app/TodoBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class TodoBoard extends React.Component {
+
+	render() {
+		return <h2>I'm the TODO Board! </h2>
+	}
+
+}
+
+export default TodoBoard;
---
+++ src/client/app/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {render} from 'react-dom';
+import TodoBoard from './TodoBoard.jsx';
+
+class App extends React.Component {
+	render () {
+		return (
+			<div>
+				<h1>React TODO Board</h1>
+				<TodoBoard />
+			</div>
+		);
+	}
+}
+
+render(<App/>, document.getElementById('app'));
